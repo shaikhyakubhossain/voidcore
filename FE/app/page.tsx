28b23c022af7b5f4e23b064b2723892e7b8bb5ae
Yakub Hossain <shaikhyakubhossain@gmail.com>
2026-07-17 21:46:10 +0530

    feat: wrap Chat component in Suspense for improved loading handling

--- FE/app/page.tsx
+++ FE/app/page.tsx
@@ -2,13 +2,14 @@
 import { useNewChatNavigation } from "@/hooks/useNewChatNavigation";
 import { JSX } from "react/jsx-runtime";
 import Chat from "@/components/Chat/Chat";
+import { Suspense } from "react";
 
 export default function Home(): JSX.Element {
   useNewChatNavigation();
 
   return (
-    <main className="w-full">
+    <Suspense fallback={null}>
       <Chat />
-    </main>
+    </Suspense>
   );
 }
